418ef05db67a3b801d7f3017c7a005736bd151e8
Hugh Burgess <hugh.burgess@2017-042019.fritz.box>
2022-12-23 17:33:01 +0100

    slow animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,27 +26,37 @@ const IndexPage = () => {
     const CANVAS_HEIGHT = (canvas.height = 600)
     const SPRITE_WIDTH = 575 // width of svg of player divided by total columns in images + 2px margin
     const SPRITE_HEIGHT = 523 // height of svg of player divided by total rows in images
-    const playerImage = new Image()
-    playerImage.src = dog
+    const PLAYER_IMAGE = new Image()
+    PLAYER_IMAGE.src = dog
+    let frameX = 0
+    let frameY = 0
+
+    // slow the animation down
+    let gameFrame = 0
+    const STAGGER_FRAMES = 3 // lower is faster, higher is slower
 
     function animate() {
       ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT) // clears the svg image between looping images
       // create a simple rectangle with positionX, positionY, width, height
       // ctx.fillRect(50, 50, 100, 100) // x here is the positionX, and then with x++ we iterate by 1 per loop
 
-      // ctx.drawImage(playerImage, sourceImageX, sourceImageY, sourceImageWidth, sourceImageHeight, destinationX, destinationY, destinationWidth, destinationHeight)
+      // ctx.drawImage(PLAYER_IMAGE, sourceImageX, sourceImageY, sourceImageWidth, sourceImageHeight, destinationX, destinationY, destinationWidth, destinationHeight)
       ctx.drawImage(
-        playerImage,
-        0,
-        0,
+        PLAYER_IMAGE,
+        frameX * SPRITE_WIDTH, // moving the width from 1 frame to the next by the width of the image
+        frameY * SPRITE_HEIGHT,
         SPRITE_WIDTH,
         SPRITE_HEIGHT,
         0,
         0,
-        SPRITE_WIDTH,
-        SPRITE_HEIGHT,
+        SPRITE_WIDTH, // setting it here to the original width of the single cropped image
+        SPRITE_HEIGHT, // setting it here to the original height of the single cropped image
       )
+      if (gameFrame % STAGGER_FRAMES === 0) {
+        if (frameX < 6) {frameX++} else frameX = 0
+      }
 
+      gameFrame++
       requestAnimationFrame(animate) // built in, runs once, but adding "animate" runs the parent funciton and thus a loop
     }
     animate() // calls the function and renders the shape
