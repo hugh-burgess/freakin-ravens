4d3bea0823a7fec73480c4441070b35ba27a5b64
Hugh Burgess <hugh.burgess@2017-042019.fritz.box>
2022-12-27 09:52:32 +0100

    trim

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,64 +1,22 @@
 import React from 'react'
 import '@styles/style.css'
-//import Dog from '@animations/dog'
-//import Background from '@animations/background'
-// import EnemyOne from '@animations/enemyOne'
-// import EnemyTwo from '@animations/enemyTwo'
-// import EnemyThree from '@animations/enemyThree'
-// import EnemyFour from '@animations/enemyFour'
-//import Explosions from '@animations/explosion'
 import PointAndShoot from '@animations/pointAndShoot'
 
 const IndexPage = () => {
   setTimeout(() => {
-    //Background()
-    // Dog()
-    // EnemyOne()
-    // EnemyTwo()
-    // EnemyThree()
-    // EnemyFour()
-    //Explosions()
     PointAndShoot()
   }, 10)
 
   return (
-    <main className="pageStyles">
-      {/* <h1 className="headingStyles">Dog Animations</h1> */}
+    <main>
       <div id="container">
-        {/* <canvas id="canvasForBackground"></canvas> */}
-        {/* <canvas id="canvasForDog"></canvas> */}
-        {/* <canvas id="canvasForEnemyOne"></canvas> */}
-        {/* <canvas id="canvasForEnemyTwo"></canvas> */}
-        {/* <canvas id="canvasForEnemyThree"></canvas> */}
-        {/* <canvas id="canvasForEnemyFour"></canvas> */}
-        {/* <canvas id="canvasForExplosions"></canvas>{' '} */}
         <canvas id="collisionCanvas"></canvas>
         <canvas id="canvasForPointAndShoot"></canvas>
-        {/* <p>
-          Game speed: <span id="showGameSpeed"></span>
-        </p> */}
-        {/* <input type="range" min="0" max="20" className="slider" id="slider" /> */}
       </div>
-      {/* <div className="controls">
-        <label for="animations">Choose animation: 
-        <select id="animations" name="animations">
-        <option value="idle">Idle</option>
-        <option value="jump">Jump</option>
-        <option value="fall">Fall</option>
-        <option value="run">Run</option>
-        <option value="dizzy">Dizzy</option>
-        <option value="rest">Rest</option>
-        <option value="roll">Roll</option>
-        <option value="bite">Bite</option>
-        <option value="dead">Dead</option>
-        <option value="hit">Hit</option>
-        </select>
-        </label>
-      </div> */}
     </main>
   )
 }
 
 export default IndexPage
 
-export const Head = () => <title>Game</title>
+export const Head = () => <title>Freakin' Ravens!</title>
